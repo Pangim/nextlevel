b81118972da7f83badfd248fa3ce3c972d741a51
박현우 <pang@baghyeon-uui-MacBookAir.local>
2022-01-10 15:03:32 +0900

    [추가]:api/category - getCategory: 카테고리 목록 가져오기 기능 - getProduct: 카테고리에 맞는 상품 페이지 가져오기 - getProductOne: 상품 하나의 정보 가져오기

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,22 +1,64 @@
 'use strict';
+const limit = 5
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-const find = async ctx => {
+
+const getCategory = async ctx => {
     try {
-        const {name} = ctx.request.query
-        const categoryId = await strapi.query('category').findOne({name: name}, ['product'])
-        const products = await categoryId.product.map( product => {
-            return {name: product.name, pirce: product.price }
+        const categoryName = await strapi.query('category').find()
+        const data = await categoryName.map( category => { 
+            return { name : category.name }
         })
         ctx.send({
-            products : products
+            data : data
         }, 200)
     } catch(error) {
         console.log(error)
     }
 }
 
-module.exports = {find};
+const getProduct = async ctx => {
+    try {
+        const { name, page } = await ctx.request.query
+        const { findProduct }  = await strapi.services.category
+        const offset = limit * page
+        const data = await findProduct(name, offset, limit)
+        if (data.length === 0){
+            ctx.send({
+                message: "does not exist page"
+            }, 404)
+        }
+        else {
+            ctx.send({
+                data: data
+            }, 200)
+        }
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+const getProductOne = async ctx => {
+    try{
+        const productCode = await ctx.params.product_code
+        const data = await strapi.query('product').findOne({product_code: productCode},[])
+        if (data === null){
+            ctx.send({
+                message : 'does not exist product code!'
+            }, 404)
+        }
+        else {
+            ctx.send({
+            data : data
+        }, 200)
+    }
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+
+module.exports = { getProduct, getCategory, getProductOne };
