96345a1b3ca2dd82a081bd653216b007c032bac8
박현우 <pang@baghyeon-uui-MacBookAir.local>
2022-01-13 12:47:29 +0900

    [수정]:api/category - services/error.js에서 에러 처리 분리 - services/category.js에서 데이터베이스 부분 분리 - 코드 정리

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,62 +1,71 @@
 'use strict';
+
 const limit = 5
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
 const getCategory = async ctx => {
+    const { findCategory } = strapi.services.category
+    const { responseParser } = strapi.services.common 
+    const categories = await findCategory()
     try {
-        const categoryName = await strapi.query('category').find()
-        const data = await categoryName.map( category => { 
+        const categoryName = await categories.map( category => { 
             return { name : category.name }
         })
-        ctx.send({
-            data : data
-        }, 200)
+        
+        return responseParser({
+            categoryName
+        })
     } catch(error) {
         console.log(error)
     }
 }
 
 const getProduct = async ctx => {
+    const { name, page } = ctx.request.query
+    const { findProduct }  = strapi.services.category
+    const { responseParser } = strapi.services.common
+    const { errorHandler, REQUEST_CATEGORY_ERROR } = require('../services/error')
+    const { NOT_FOUND_PRODUCTS } = REQUEST_CATEGORY_ERROR
+    const offset = limit * page
+    const getProduct = await findProduct(name)
+    const products = await getProduct.product.slice(offset-limit, offset).map( product => {
+        return {name: product.name, pirce: product.price + "원" }
+    })
+
     try {
-        const { name, page } = await ctx.request.query
-        const { findProduct }  = await strapi.services.category
-        const offset = limit * page
-        const data = await findProduct(name, offset, limit)
-        if (data.length === 0){
-            ctx.send({
-                message: "does not exist page"
-            }, 404)
-        }
-        else {
-            ctx.send({
-                data: data
-            }, 200)
-        }
+        if (products.length === 0) throw Error(NOT_FOUND_PRODUCTS)
+
+        return responseParser({
+            products
+        })
     } catch(error) {
         console.log(error)
+        return ctx.badRequest(errorHandler(error.message))
     }
 }
 
 const getProductOne = async ctx => {
+    const productCode = ctx.params.product_code
+    const { getProduct } = strapi.services.category
+    const { responseParser } = strapi.services.common
+    const { errorHandler, REQUEST_CATEGORY_ERROR } = require('../services/error')
+    const { NOT_FOUND_PRODUCT } = REQUEST_CATEGORY_ERROR
+    const product = await getProduct(productCode)
+
     try{
-        const productCode = await ctx.params.product_code
-        const data = await strapi.query('product').findOne({product_code: productCode},[])
-        if (data === null){
-            ctx.send({
-                message : 'does not exist product code!'
-            }, 404)
+        if (product === null) throw Error(NOT_FOUND_PRODUCT)
+        const productInfo = {
+            "상품 이름": product.name,
+            "상품 정보": product.information,
+            "상품 가격": product.price,
+            "상품 개수": product.quantitiy
         }
-        else {
-            ctx.send({
-            data : data
-        }, 200)
-    }
+
+        return responseParser({
+            productInfo
+        })
     } catch(error) {
         console.log(error)
+        ctx.badRequest(errorHandler(error.message))
     }
 }
 
--- api/category/services/category.js
+++ api/category/services/category.js
@@ -1,14 +1,17 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
- const findProduct = async (name, offset, limit)=> {
-        const categoryId = await strapi.query('category').findOne({name: name}, ['product'])
-        const products = await categoryId.product.slice(offset-limit, offset).map( product => {
-            return {name: product.name, pirce: product.price + "원" }
-        })
-        return products
+ const findProduct = async name => {
+        const getPorduct = await strapi.query('category').findOne({name: name}, ['product'])
+        return getPorduct
     }
-module.exports = {findProduct};
+
+const findCategory = async => {
+    const categoryName = strapi.query('category').find()
+    return categoryName
+}
+
+const getProduct = async productCode => {
+    const product = await strapi.query('product').findOne({product_code: productCode},[])
+    return product
+}
+module.exports = { findProduct, findCategory, getProduct };
